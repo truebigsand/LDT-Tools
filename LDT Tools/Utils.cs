9f60819b587168ade4c147178a74cbf2eed135ef
truebigsand <3244346642@qq.com>
2022-08-16 17:26:02 +0800

    Add: Basic Framework

---
+++ LDT Tools/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using LDT_Tools.Tools;
+
+namespace LDT_Tools
+{
+    public static class Utils
+    {
+        public static void ConsoleWriteErrorLine(string Message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(Message);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        public static string GetCustomOrDefaultName(Type type)
+        {
+            var nameAttribute = type.GetCustomAttribute<DisplayNameAttribute>();
+            if (nameAttribute == null)
+            {
+                return type.Name;
+            }
+            return nameAttribute.Name;
+        }
+    }
+}
