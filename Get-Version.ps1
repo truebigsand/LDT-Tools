1c9964759166d171c318af629ae436fe0004d898
truebigsand <3244346642@qq.com>
2022-11-05 21:41:04 +0800

    Fix: CI release

--- Get-Version.ps1
+++ Get-Version.ps1
@@ -2,11 +2,11 @@ $Content = Get-Content ./RELEASE_VERSION
 $Tokens = $Content.Split(' ')
 $ReleaseVersion = $Tokens[0]
 if ($Tokens.Count.Equals(2)) {
-    $PreRelease = 1
+    Add-Content -Path $env:GITHUB_ENV -Value "PreRelease=true"
 } else {
-    $PreRelease = 0
+    Add-Content -Path $env:GITHUB_ENV -Value "PreRelease=false"
 }
 Add-Content -Path $env:GITHUB_ENV -Value "ReleaseVersion=${ReleaseVersion}"
-Add-Content -Path $env:GITHUB_ENV -Value "PreRelease=${PreRelease}"
+
 # echo $ReleaseVersion
 # echo $PreRelease
