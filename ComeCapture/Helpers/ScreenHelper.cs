167972f950cf4fa68b0a9027e7898bddbb59fcbf
wuxianao <100472806+wuxianao@users.noreply.github.com>
2022-11-06 16:45:20 +0800

    Add files via upload

---
+++ ComeCapture/Helpers/ScreenHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace ComeCapture.Helpers
+{
+    public static class ScreenHelper
+    {
+        [DllImport("user32.dll", EntryPoint = "ReleaseDC")]
+        public static extern IntPtr ReleaseDC(
+            IntPtr hWnd,
+            IntPtr hDc
+            );
+
+        [DllImport("gdi32.dll")]
+        public static extern int GetDeviceCaps(
+            IntPtr hdc,
+            int nIndex 
+            );
+
+        public static System.Drawing.Size GetPhysicalDisplaySize()
+        {
+            Graphics g = Graphics.FromHwnd(IntPtr.Zero);
+            IntPtr desktop = g.GetHdc();
+            int physicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.Desktopvertres);
+            int physicalScreenWidth = GetDeviceCaps(desktop, (int)DeviceCap.Desktophorzres);
+            ReleaseDC(IntPtr.Zero, desktop);
+            g.Dispose();
+            return new System.Drawing.Size(physicalScreenWidth, physicalScreenHeight);
+        }
+
+        public enum DeviceCap
+        {
+            Desktopvertres = 117,
+            Desktophorzres = 118
+        }
+
+        public static void ResetScreenScale()
+        {
+            using (var g = Graphics.FromHwnd(IntPtr.Zero))
+            {
+                IntPtr desktop = g.GetHdc();
+                int physicalScreenWidth = GetDeviceCaps(desktop, (int)DeviceCap.Desktophorzres);
+                Cut.ScreenScale = physicalScreenWidth * 1.0000 / Cut.ScreenWidth;
+            }
+        }
+    }
+}
